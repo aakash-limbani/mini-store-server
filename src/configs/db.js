42e5aab06919019f57c6768f336738bca31601ef
Akash Limbani <akashlimbani22@gmail.com>
2022-12-23 15:33:14 +0530

    mini store backend added

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const { connect } = require('mongoose');
 require('dotenv').config();
 
 module.exports = () => {
-    return connect(process.env.MONGODB_URL, {
+    return connect('mongodb://127.0.0.1:27017/mini-store', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
